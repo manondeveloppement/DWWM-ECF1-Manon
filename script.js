53c8a746abb2ea2678bbeadbba15fff381098ab1
Manon <manon.goursaud33@gmail.com>
2024-11-26 16:23:19 +0100

    ajout a 16h22 le 26/11/2024

--- script.js
+++ script.js
@@ -5,4 +5,16 @@
    burger.addEventListener('click', () => {
      navButtons.classList.toggle('navbar__buttons--active'); // Toggle class to show/hide links
    });
-   
+   const audioPlayer = document.getElementById("audioPlayer");
+      const playButton = document.getElementById("playButton");
+
+      // Toggle play/pause when the button is clicked
+      playButton.addEventListener("click", function () {
+        if (audioPlayer.paused) {
+          audioPlayer.play();
+          playButton.textContent = "⬛"; // Change button text to "Pause"
+        } else {
+          audioPlayer.pause();
+          playButton.textContent = "▶"; // Change button text to "Play"
+        }
+      });
